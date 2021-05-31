2282869b999f3e155f9878272f219103b376a4f8
Chyngyz Omorov <chyngyz1996@gmail.com>
2021-05-30 22:00:31 -0400

    Added WebSocket Server

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require("http");
+const WebSocketServer = require("websocket").server;
+let connection = null;
 
 const httpserver = http.createServer((req, res) => {
-
     console.log("we have received a request");
-})
+});
+
+const websocket = new WebSocketServer({
+    "httpServer": httpserver
+});
+
+websocket.on("request", request => {
+    connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("Opened!!!"));
+    connection.on("closed", () => console.log("Closed!!!"));
+    connection.on("onmessage", message => {
+        console.log(`Received message ${message}`);
+    });
+});
 
 httpserver.listen(8080, () => console.log("My server is listening on port 8080"));
